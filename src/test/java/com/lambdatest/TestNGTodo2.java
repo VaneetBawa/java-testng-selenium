4ddbb90cea6f9b3514a7a6d407903b3a1b1206dd
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2025-05-19 19:26:41 +0530

    Update TestNGTodo2.java

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -41,7 +41,7 @@ public class TestNGTodo2 {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("build",  buildname);
         caps.setCapability("platform", System.getenv("LT_PLATFORM"));
-        caps.setCapability("browserName", System.getenv("LT_BROWSER_NAME"));
+        caps.setCapability("browserName", System.getenv("LT_BROWSERS"));
         caps.setCapability("version", System.getenv("LT_BROWSER_VERSION"));
       //  caps.setCapability("build", "TestNG With Java");
         caps.setCapability("name", m.getName() + this.getClass().getName());
