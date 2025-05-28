c81bd75b60cf18a2ab2177b29d985bfde7df01ba
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2025-05-28 23:02:32 +0530

    Update TestNGTodo2.java

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -66,7 +66,7 @@ public class TestNGTodo2 {
             options.setCapability("LT:Options", ltOptions);
             driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), options);
 
-        } else if (browserName.equals("edge")) {
+        } else if (browserName.equals("MicrosoftEdge")) {
             EdgeOptions options = new EdgeOptions();
             options.setPlatformName(platform);
             options.setBrowserVersion(browserVersion);
